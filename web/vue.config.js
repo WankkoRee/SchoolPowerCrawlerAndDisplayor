5d9c465832abe4d4ed494cbee103cbba45d7998a
WankkoRee <wkr@wkr.moe>
2022-03-07 08:53:55 +0800

    feat: 拆分资源打包

--- web/vue.config.js
+++ web/vue.config.js
@@ -7,6 +7,33 @@ module.exports = defineConfig({
       return args
     })
   },
+  configureWebpack: config => {
+    config.optimization = {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        cacheGroups: {
+          vendor: {
+            name (module) {
+              if (/[\\/]node_modules[\\/]/.test(module.context)) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                // console.log(`npm.${packageName.replace('@', '')}`, module.context)
+                return `npm.${packageName.replace('@', '')}`
+              } else {
+                // console.log('main', module.context)
+                return 'main'
+              }
+            }
+          }
+        }
+      }
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
