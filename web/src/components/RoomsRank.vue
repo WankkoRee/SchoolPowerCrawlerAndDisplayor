b1fb235ffc618e81b45e0f4aeddbd168d72d1815
Wankko Ree <wkr@wkr.moe>
2023-02-21 19:01:48 +0800

    fix: 优化了排行榜的显示

--- web/src/components/RoomsRank.vue
+++ web/src/components/RoomsRank.vue
@@ -4,8 +4,8 @@
       <span>
         {{ {0: '🥇', 1: '🥈', 2:'🥉'}[index] || '' }} {{ ["area", "building", "room"].includes(type)?roomData.area:'' }}{{ ["building", "room"].includes(type)?` > ${roomData.building}`:'' }}{{ ["room"].includes(type)?` > ${roomData.room}`:'' }}
       </span>
-      <n-progress type="line" :processing="true" :color="{0: '#ffd666', 1: '#bfbfbf', 2: '#d46b08'}[index] || '#66ccff'" indicator-placement="outside" :percentage="roomData.spending / data[0].spending * 100">
-        {{ roomData.spending }} {{ unit }}
+      <n-progress type="line" :processing="true" :color="{0: '#ffdd80', 1: '#cccccc', 2: '#d49d6a'}[index] || '#66ccff'" indicator-placement="outside" :percentage="roomData.spending / data[0].spending * 100">
+        {{ roomData.spending.toFixed(2) }} {{ unit }}
       </n-progress>
     </n-grid-item>
   </n-grid>
