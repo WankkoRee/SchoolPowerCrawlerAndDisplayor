fe59a78c37b0e1bdb775db1a54a48ae80e93801e
Wankko Ree <wkr@wkr.moe>
2023-02-21 13:48:26 +0800

    fix: 更换一处文本显示

--- web/src/components/RoomsRank.vue
+++ web/src/components/RoomsRank.vue
@@ -2,7 +2,7 @@
   <n-grid :x-gap="8" :y-gap="8" cols="1">
     <n-grid-item v-for="(roomData, index) in data" :key="index">
       <span>
-        {{ {0: '🥇', 1: '🥈', 2:'🥉'}[index] || '' }} {{ ["area", "building", "room"].includes(type)?roomData.area:'' }}{{ ["building", "room"].includes(type)?` の ${roomData.building}`:'' }}{{ ["room"].includes(type)?` の ${roomData.room}`:'' }}
+        {{ {0: '🥇', 1: '🥈', 2:'🥉'}[index] || '' }} {{ ["area", "building", "room"].includes(type)?roomData.area:'' }}{{ ["building", "room"].includes(type)?` / ${roomData.building}`:'' }}{{ ["room"].includes(type)?` / ${roomData.room}`:'' }}
       </span>
       <n-progress type="line" :processing="true" :color="{0: '#ffd666', 1: '#bfbfbf', 2: '#d46b08'}[index] || '#66ccff'" indicator-placement="outside" :percentage="roomData.spending / data[0].spending * 100">
         {{ roomData.spending }} {{ unit }}
